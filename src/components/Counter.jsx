c66d44d170d38b73ab01736a76937547bb40b270
mathieuMSdos <mathieumsdos@gmail.com>
2022-09-01 09:21:34 +0200

    little bug fixed

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Counter() {
+  return (
+    <section className="counterContainer">
+        <div className="counterElement1"><h2>1000</h2></div>
+        <div className="counterElement2"><h2>2000</h2></div>
+        <div className="counterElement3"><h2>3000</h2></div>
+        <div className="counterElement4"><h2>4000</h2></div>
+    </section>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Separator from "../components/Separator";
 import VideoHero from "../components/VideoHero";
+import Counter from "../components/Counter"
 
 export default function home() {
   return (
@@ -9,30 +10,8 @@ export default function home() {
       <Navbar />
       <main>
         <VideoHero />
-        {/* <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-          suscipit dolores illum praesentium enim deleniti quos odit voluptate
-          quod. Nihil architecto et iste eligendi minima repellat veniam labore,
-          similique neque possimus perspiciatis nesciunt nisi necessitatibus
-          quasi odio ab iure totam vero dicta? Atque, animi asperiores odit
-          veritatis totam voluptate minus maxime. Officia reiciendis quod
-          officiis id quo qui ullam quibusdam in, earum, fugiat modi
-          necessitatibus doloribus nesciunt optio asperiores beatae iure iusto
-          aperiam molestias et dolor nobis voluptatem pariatur. Fugiat possimus
-          libero sint hic atque odio? Doloremque exercitationem suscipit ratione
-          perferendis doloribus, vitae, vero sunt enim deserunt vel culpa.
-          Recusandae, aut iusto quod fugit impedit perspiciatis ut consequatur
-          repellat quo, nisi error nemo molestiae incidunt quos excepturi itaque
-          laboriosam voluptate numquam aliquid! Quia sunt earum odit illo id.
-          Nemo veniam odit voluptas hic quidem nihil eaque similique unde rem
-          nam id voluptate, consequatur debitis! Magni doloribus culpa, neque
-          similique magnam hic fugiat, eveniet corrupti molestias delectus,
-          necessitatibus commodi aliquid consectetur adipisci quaerat ipsa ipsam
-          natus rerum voluptatem quo numquam! Laboriosam dolore natus sed totam
-          saepe. Eum veritatis perspiciatis, vel nostrum sapiente itaque maiores
-          qui? Numquam, accusantium molestiae tempora distinctio iste possimus?
-          Facilis ea incidunt asperiores iure at odit, tenetur nemo. lorem200
-        </p> */}
+        <Separator></Separator>
+        <Counter />
 
       </main>
     </>
