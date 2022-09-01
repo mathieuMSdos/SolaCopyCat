4f8b83b371ae61c12315fa1bbabe492f97279d08
mathieuMSdos <mathieumsdos@gmail.com>
2022-09-01 14:42:55 +0200

    counter basics WORKS. Need style now

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function Counter() {
+  const [count, setCount] = useState(95157500000);
+  const [showCount, setShowCount] = useState("")
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+  useEffect(() => {
+    setInterval(() => {
+      setCount((count) => count + 103);
+    }, 10);
+  }, []);
+
+  useEffect(() => {
+    let result = numberWithCommas(count)
+    setShowCount(result)
+  },[count])
+
+
   return (
     <section className="counterContainer">
-        <div className="counterElement1"><h2>1000</h2></div>
-        <div className="counterElement2"><h2>2000</h2></div>
-        <div className="counterElement3"><h2>3000</h2></div>
-        <div className="counterElement4"><h2>4000</h2></div>
+      <div className="counterElement1">
+        <h2>1000</h2>
+      </div>
+      <div className="counterElement2">
+        <h2>{showCount}</h2>
+      </div>
+      <div className="counterElement3">
+        <h2>3000</h2>
+      </div>
+      <div className="counterElement4">
+        <h2>4000</h2>
+      </div>
     </section>
-  )
+  );
 }
