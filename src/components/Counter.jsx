d93e1564bfd8ef956f5793c69549e4b487adfbfa
mathieuMSdos <mathieumsdos@gmail.com>
2022-09-02 23:22:16 +0200

    counter component OK and reponsive

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,36 +2,52 @@ import React, { useState, useEffect } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(95157500000);
-  const [showCount, setShowCount] = useState("")
+  const [showCount, setShowCount] = useState("");
 
   function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
   useEffect(() => {
     setInterval(() => {
-      setCount((count) => count + 103);
+      setCount((count) => count + 10);
     }, 10);
   }, []);
 
   useEffect(() => {
-    let result = numberWithCommas(count)
-    setShowCount(result)
-  },[count])
-
+    let result = numberWithCommas(count);
+    setShowCount(result);
+  }, [count]);
 
   return (
     <section className="counterContainer">
-      <div className="counterElement1">
-        <h2>1000</h2>
+      <div className="containerElement">
+        <div className="counterElement">
+          <h3>Transactions per second</h3>
+          <p>2,339</p>
+        </div>
       </div>
-      <div className="counterElement2">
-        <h2>{showCount}</h2>
+
+      <div className="containerElement">
+        <div className="counterElement">
+          <h3>Total Transactions</h3>
+          <div className="transacCounter">
+            <p className="counter">{showCount}</p>
+          </div>
+        </div>
       </div>
-      <div className="counterElement3">
-        <h2>3000</h2>
+
+      <div className="containerElement">
+        <div className="counterElement">
+          <h3>Avg. cost per transaction</h3>
+          <p>$0.00025</p>
+        </div>
       </div>
-      <div className="counterElement4">
-        <h2>4000</h2>
+
+      <div className="containerElement">
+        <div className="counterElement">
+          <h3>Validator nodes</h3>
+          <p>1,948</p>
+        </div>
       </div>
     </section>
   );
