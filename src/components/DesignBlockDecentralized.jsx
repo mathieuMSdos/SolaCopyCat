9f38fdcf18857348be5ce0caa1e8c0724a6529d6
mathieuMSdos <mathieumsdos@gmail.com>
2022-09-05 13:06:41 +0200

    design Decentralized Block OK, reponsive OK

--- src/components/DesignBlockDecentralized.jsx
+++ src/components/DesignBlockDecentralized.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import decentralizedImg from "../assets/img/index_decentralized.webp";
 
 export default function DesignBlockDecentralized() {
   return (
-    <div className='centralizedBlockContainer'>DesignBlockDecentralized</div>
-  )
+    <section className="centralizedBlockContainer">
+        <img src={decentralizedImg} alt="" />
+      <div className="content">
+        <h2>Decentralized</h2>
+        <h3>and secure</h3>
+        <h4>
+          Not only is Solana ultra-fast and low cost, it is censorship
+          resistant. The Solana network is spread over thousands of independent
+          nodes — which means that your transactions are always safe.
+        </h4>
+      </div>
+    </section>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,6 +44,10 @@ export default function Home() {
           ))}
         </section>
         <DesignBlockDecentralized />
+        <Separator></Separator>
+        <Separator></Separator>
+
+
       </main>
     </>
   );
